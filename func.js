c03e5fc3b5a1415f282409d901587d7ed14ce840
OZv <voc_bt@yahoo.co.jp>
2016-04-16 12:14:00 +0800

    Update func.js

--- func.js
+++ func.js
@@ -31,14 +31,39 @@ var x=parseInt(b.getAttribute("data-index"),10);
 var h="",c,e,k;
 for(var i=0;i<vo[x][n].length;i++){
 c=vo[x][n][i];
+//console.info(c);
 k=cl[c];
 e=c.substr(0,1)=="6"?"SH":"SZ";
-h+='<tr><td>'+(i+1)+'</td><td><a href="http://stock.jrj.com.cn/share,'+c+'.shtml" target="_blank">'+c+'</a></td><td><a href="http://www.windin.com/home/stock/html/'+c+'.'+e+'.shtml" target="_blank">'+k[0]+'</a></td><td class="jyn">'+p(vc[c][0])+"</td><td align=right>"+parseFloat(vc[c][1]).toFixed(2)+"</td>"+j(vc[c],n)+"<td>"+k[1]+"</td><td>"+k[2]+"</td></tr>";
+h+='<tr><td>'+(i+1)+'</td><td><a href="http://gupiao.baidu.com/stock/'+e.toLowerCase()+c+'.html" target="_blank">'+c+'</a></td><td><a href="http://www.windin.com/home/stock/html/'+c+'.'+e+'.shtml" target="_blank">'+k[0]+'</a></td><td class="jyn">'+p(vc[c][0])+"</td><td align=right>"+parseFloat(vc[c][1]).toFixed(2)+"</td>"+j(vc[c],n)+"<td>"+k[1]+"</td><td>"+k[2]+"</td></tr>";
 }
 b.innerHTML=h;
 }
+function g6z(){
+var q=qg["quotation"],a,h="",c;
+for(var i=0;i<q.length;i++){
+a=q[i].split(",");
+if(a[11].substr(0,1)=="-")c='"ixm"'; else c='"ixp"';
+h+='<span class="qgi">'+a[2]+'</span><span class='+c+'>'+a[5]+'</span><span class='+c+'>'+a[11]+'</span>';
+}
+console.info(h);
+var w=document.createElement("div");
+w.className="qgd";
+w.innerHTML=h;
+var u=document.getElementsByTagName("div");
+for(var i=0;i<u.length;i++)
+if(u[i].className=="l7s"){
+u[i].parentNode.insertBefore(w,u[i]);
+break;
+}
+}
 function k9y(a,n){
 if(a&&!u(a,"nwl"))return;
+if(!a){
+var s=document.createElement("script");
+s.setAttribute("src","http://hq2gjgp.eastmoney.com/EM_Quote2010NumericApplication/Index.aspx?reference=rtj&Type=Z&jsName=qg&ids=INDU7,CCMP7,NKY7");
+s.setAttribute("onload","g6z()");
+document.getElementsByTagName("head")[0].appendChild(s);
+}
 var b=document.getElementsByTagName("tbody")[0];
 b.setAttribute("data-index",n);
 e8c(null,0);
