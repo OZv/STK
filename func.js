de00a6fde35aca1fd6e254e80fc1651662cba789
OZv <voc_bt@yahoo.co.jp>
2016-04-30 14:55:12 +0800

    Update func.js

--- func.js
+++ func.js
@@ -3,6 +3,21 @@ var d=parseFloat(n);
 var c=d<0?"czx":(d==0?"psz":"yuv");
 return '<span class="'+c+'">'+d.toFixed(2)+'%</span>';
 }
+function q(a,b){
+var c=b<0?"czx":(b==0?"psz":"yuv");
+return '<td class="aqn">'+a+'</td><td class="vvn">(<span class="'+c+'">'+b+'</span>)</td>';
+}
+function b(d,c,h){
+var w=document.createElement(d);
+w.className=c;
+w.innerHTML=h;
+var u=document.getElementsByTagName("div");
+for(var i=0;i<u.length;i++)
+if(u[i].className=="buc"){
+u[i].insertBefore(w,u[i].childNodes[0]);
+break;
+}
+}
 function j(v,n){
 var h="",t;
 for(var i=2;i<v.length;i+=2){
@@ -26,7 +41,7 @@ return true;
 function e8c(a,n){
 var b=document.getElementById("tb8").childNodes[n];
 if(!u(b,"oxc")&&a)return;
-b=document.getElementsByTagName("tbody")[0];
+b=document.getElementById("ttg");
 var x=parseInt(b.getAttribute("data-index"),10);
 var h="",c,e,k;
 for(var i=0;i<vo[x][n].length;i++){
@@ -45,16 +60,7 @@ a=q[i].split(",");
 if(a[11].substr(0,1)=="-")c='"ixm"'; else c='"ixp"';
 h+='<span class="qgi">'+a[2]+'</span><span class='+c+'>'+a[5]+'</span><span class='+c+'>'+a[11]+'</span>';
 }
-console.info(h);
-var w=document.createElement("div");
-w.className="qgd";
-w.innerHTML=h;
-var u=document.getElementsByTagName("div");
-for(var i=0;i<u.length;i++)
-if(u[i].className=="l7s"){
-u[i].parentNode.insertBefore(w,u[i]);
-break;
-}
+b("div","qgd",h);
 }
 function k9y(a,n){
 if(a&&!u(a,"nwl"))return;
@@ -64,12 +70,22 @@ s.setAttribute("src","http://hq2gjgp.eastmoney.com/EM_Quote2010NumericApplicatio
 s.setAttribute("onload","g6z()");
 document.getElementsByTagName("head")[0].appendChild(s);
 }
-var b=document.getElementsByTagName("tbody")[0];
+var b=document.getElementById("ttg");
 b.setAttribute("data-index",n);
 e8c(null,0);
 }
 function d(){
+o6m();
 k9y(null,0);
+}
+function o6m(){
+if(typeof(ft)!="undefined"){
+var h='';
+for(var k in ft){
+h+='<tr><td>'+k+' = &#x591A;: </td>'+q(ft[k][0],ft[k][1])+'<td class="b18">&#x7A7A;: </td>'+q(ft[k][2],ft[k][3])+'<td class="b18">&#x51C0;&#x591A;: </td>'+q(ft[k][4],ft[k][5])+'<td class="b18">&#x51C0;&#x7A7A;: </td>'+q(ft[k][6],ft[k][7])+'</tr>';
+}
+b("table","fed",h);
+}
 }
 if(window.addEventListener)
 window.addEventListener("load",d,false);
